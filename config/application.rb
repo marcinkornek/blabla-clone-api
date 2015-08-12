6ebbd59c4ba025de60eb76dc90b0ed14fe7bd352
marcinkornek <marcinkornek@gmail.com>
2015-08-12 16:43:07 +0200

    Add CORS config

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,15 @@ module BlablaCloneApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*' # change later to front end url
+        # location of your API
+        resource '/api/*',
+          :headers => :any,
+          :methods => [:get, :post, :options, :put, :delete]
+      end
+    end
   end
 end
