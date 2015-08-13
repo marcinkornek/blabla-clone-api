a574ebff359039c37f7ad7a6dd3866f4b8004cea
marcinkornek <marcinkornek@gmail.com>
2015-08-13 08:44:06 +0200

    Add login with email & password, add logout

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -10,6 +10,24 @@ module API
         error_response(message: e.message, status: 404)
       end
 
+      helpers do
+        def authenticate!
+          error!('Unauthorized. Wrong email and/or invalid or expired token.', 401) unless current_user
+        end
+
+        def token
+          @token ||= user.tokens.find_by(access_token: headers['X-User-Token']) if user
+        end
+
+        def user
+          @user ||= User.find_by(email: headers['X-User-Email'])
+        end
+
+        def current_user
+          @current_user ||= User.find(token.user_id) if token && !token.expired?
+        end
+      end
+
       mount API::V1::Sessions
     end
   end
--- app/controllers/api/v1/sessions.rb
+++ app/controllers/api/v1/sessions.rb
@@ -3,7 +3,7 @@ module API
     class Sessions < Grape::API
       resource :sessions do
 
-        desc "Authenticate user with oath and return user object / access token"
+        desc "Authenticate user with oath and return user access token"
         params do
           requires :uid,        type: String, desc: "User uid"
           requires :provider,   type: String, desc: "User provider"
@@ -13,16 +13,45 @@ module API
         end
         post :oath_login do
           auth = {}
-          auth[:uid] = params[:uid]
-          auth[:provider] = params[:provider]
-          auth[:email] = params[:email]
+          auth[:uid]        = params[:uid]
+          auth[:provider]   = params[:provider]
+          auth[:email]      = params[:email]
           auth[:first_name] = params[:first_name]
-          auth[:last_name] = params[:last_name]
+          auth[:last_name]  = params[:last_name]
           user = User.find_for_oauth(auth)
           if user
             user.tokens.create.extend(TokenRepresenter)
           else
             'there is no such user'
+          end
+        end
+
+        desc "Authenticate user with email and password and return user access token"
+        params do
+          requires :email,    type: String, desc: "User email"
+          requires :password, type: String, desc: "User password"
+        end
+        post :login do
+          email    = params[:email]
+          password = params[:password]
+
+          user = User.where(email: email.downcase).first
+          error!({error: 'Invalid Email and/or Password'},401) if user.nil?
+          if user.valid_password?(password)
+            user.tokens.create.extend(TokenRepresenter)
+          else
+            'there is no such user'
+          end
+        end
+
+        desc "Destroy the access token"
+        delete do
+          authenticate!
+          if token
+            token.destroy
+            {ok: 'logged out'}
+          else
+            error!({error: 'Invalid access token.'}, 401)
           end
         end
       end
