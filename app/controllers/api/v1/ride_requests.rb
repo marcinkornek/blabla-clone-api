75c48b731f5792e8e069a4850094957a93f6a793
marcinkornek <marcinkornek@gmail.com>
2015-09-26 12:48:19 +0200

    add change ride requests status endpoint

--- app/controllers/api/v1/ride_requests.rb
+++ app/controllers/api/v1/ride_requests.rb
@@ -21,6 +21,34 @@ module API
             ride_request.errors.messages
           end
         end
+
+        desc "Change ride request status"
+        params do
+          requires :id,     type: Integer, desc: "ride request id"
+          requires :status, type: String,  desc: "ride request status"
+        end
+        route_param :id do
+          put do
+            authenticate!
+            if ride_request
+              ride_request.update(status: params[:status])
+              ride_request.ride.extend(RideShowOwnerRepresenter)
+            else
+              status 406
+              ride_request.errors.messages
+            end
+          end
+        end
+      end
+
+      helpers do
+        def ride_request
+          @ride_request ||= RideRequest.find(params[:id])
+        end
+
+        def ride_request_owner?
+          ride_request.ride.driver.id == current_user.id if current_user.present?
+        end
       end
     end
   end
