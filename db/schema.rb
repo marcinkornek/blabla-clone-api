2c26968547d607ecd7309bd1add36b7f38f18703
marcinkornek <marcinkornek@gmail.com>
2016-04-06 20:08:04 +0200

    Add column 'last_seen_at' to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904175406) do
+ActiveRecord::Schema.define(version: 20160406063523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20150904175406) do
     t.string   "birth_year"
     t.integer  "role",                   default: 0
     t.string   "avatar"
+    t.datetime "last_seen_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
