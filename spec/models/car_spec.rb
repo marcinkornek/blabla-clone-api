f3c395565337ade52aa745db4bfe3db4e20e0433
marcinkornek <marcinkornek@gmail.com>
2016-07-26 21:36:17 +0200

    Add 'Car' model specs

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:car) { FactoryGirl.create(:car, brand: 'Ford', model: 'Focus', comfort: 1) }
+
+  describe "#full_name" do
+    it "returns car brand and model" do
+      expect(car.full_name).to eq("Ford Focus")
+    end
+  end
+
+  describe "#comfort_stars" do
+    context "car comfort present" do
+      it "returns comfort stars" do
+        expect(car.comfort_stars).to eq(1)
+      end
+    end
+
+    context "car comfort is nil" do
+      before { car.update(comfort: nil) }
+
+      it "returns nil" do
+        expect(car.comfort_stars).to be_nil
+      end
+    end
+  end
 end
