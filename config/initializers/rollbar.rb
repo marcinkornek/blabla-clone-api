465fdd179e84825d17f3b29c9f58700440c50585
marcinkornek <marcinkornek@gmail.com>
2016-08-17 19:10:07 +0200

    Move Rollbar access token to application.yml

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,7 +1,6 @@
 if Rails.env.production?
   Rollbar.configure do |config|
-    config.access_token = '987fca994b2e41a1b68d9f12e709f5b9'
-    config.enabled = false if Rails.env.test? || Rails.env.development?
-    config.environment = ENV['ROLLBAR_ENV'] || Rails.env
+    config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
+    config.environment = ENV["ROLLBAR_ENV"] || Rails.env
   end
 end
