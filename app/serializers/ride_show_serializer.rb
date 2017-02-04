25378159ac4751ca8be3233edabf275f9eb37f86
marcinkornek <marcinkornek@gmail.com>
2017-02-04 17:01:21 +0100

    Add missing attribute to RideShowSerializer

--- app/serializers/ride_show_serializer.rb
+++ app/serializers/ride_show_serializer.rb
@@ -2,7 +2,7 @@
 class RideShowSerializer < ActiveModel::Serializer
   attributes :id, :free_places_count, :places, :places_full, :start_date,
              :price, :currency, :car_id, :created_at, :updated_at,
-             :user_ride_request, :ride_requests
+             :user_ride_request, :ride_requests, :requested_places_count
 
   has_one :driver, serializer: UserSimpleSerializer
   has_one :start_location, serializer: LocationSimpleSerializer
@@ -12,6 +12,11 @@ class RideShowSerializer < ActiveModel::Serializer
   def user_ride_request
     return unless scope&.current_user
     object.ride_requests.find_by(passenger_id: scope&.current_user&.id)
+  end
+
+  def requested_places_count
+    return unless scope&.current_user&.id == object.driver_id
+    object.requested_places
   end
 
   def ride_requests
