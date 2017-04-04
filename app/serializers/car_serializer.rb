84f583f8d7a2b1c89c0f89eb2ae6993e4ad1047f
marcinkornek <marcinkornek@gmail.com>
2017-04-04 11:46:54 +0200

    Add 'owner_full_name' to car serializer

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CarSerializer < ActiveModel::Serializer
-  attributes :id, :brand, :model, :full_name, :production_year, :comfort,
+  attributes :id, :brand, :model, :full_name, :owner_full_name, :production_year, :comfort,
              :comfort_stars, :places, :places_full, :color, :comfort,
              :category, :created_at, :owner_id, :car_photo
 
@@ -13,6 +13,10 @@ class CarSerializer < ActiveModel::Serializer
 
   def owner_id
     object.user_id
+  end
+
+  def owner_full_name
+    object.user.full_name
   end
 
   def places_full
